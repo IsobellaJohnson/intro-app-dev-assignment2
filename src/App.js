af86cd54dc42c3fb903fc56e1917b66891525d46
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2021-05-27 09:16:25 +1200

    initial commit
    created react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    api: null,
+  });
 
+useEffect(()=> {
+setAppState({ loading:true});
+const apiUrl = 'https://introappdev.herokuapp.com/api/movies';
+fetch(apiUrl)
+  .then((res) => res.json())
+  .then((api) => {
+setAppState({ loading: false, api: api });
+});
+}, [setAppState]);
+return (
+  <div className='App'>
+    <div className='container'>
+    </div>
+    <div className='api-container'>
+        <ListLoading isLoading={appState.loading} api={appState.api} />
+      </div>
+  </div>
+)
+}
 export default App;
