927d122c7bc9386aa62f978e64127384fd2fb195
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2021-06-01 12:02:14 +1200

    added data to table

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
+//import { useTable } from "react-table";
+import Table from './TableContainer'
 import './App.css';
-import List from './components/List';
-import withListLoading from './components/withListLoading';
 
 function App() {
-  const ListLoading = withListLoading(List);
-  const [appState, setAppState] = useState({
-    loading: false,
-    api: null,
-  });
+  const [data, setData] = useState([]);
 
-useEffect(()=> {
-setAppState({ loading:true});
-const apiUrl = 'https://introappdev.herokuapp.com/api/movies';
-fetch(apiUrl)
-  .then((res) => res.json())
-  .then((api) => {
-setAppState({ loading: false, api: api });
-});
-}, [setAppState]);
-return (
-  <div className='App'>
-    <div className='container'>
+  useEffect(() => {
+    axios("https://introappdev.herokuapp.com/api/movies")
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, []);
+
+   
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Movies",
+        columns: [
+          {
+            Header: "Title",
+            accessor: "title"
+          },
+          {
+            Header: "Genre",
+            accessor: "genre",
+            
+          },
+          {
+            Header: "Year",
+            accessor: "year"
+          },
+          {
+            Header: "Director",
+            accessor: "director"
+          },
+        ]
+      }
+    ]
+  )
+
+  return (
+    <div className="App">
+      <h1><center>Movies</center></h1>
+      <Table key={data.id} columns={columns} data={data} />
     </div>
-    <div className='api-container'>
-        <ListLoading isLoading={appState.loading} api={appState.api} />
-      </div>
-  </div>
-)
+  );
 }
+
 export default App;
