0a245b61a802ef9087c1417b5b131a293feaf5aa
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2021-06-15 12:01:23 +1200

    attempted post
    doesnt work

--- src/components/movieModal.js
+++ src/components/movieModal.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import './modal.css'
 import customStyles from './customStyles'
+import axios from "axios";
 
+//create post.js, place componentdidmount into form, import to modal? change data to the same params idk
 
   export default function MovieModal(){
     var subtitle;
@@ -11,6 +13,21 @@ import customStyles from './customStyles'
     function openModal() {
       setIsOpen(true);
     }
+
+    function postMovie(){
+      axios({
+        method: 'post',
+        url: 'https://introappdev.herokuapp.com/api/movies',
+        data: {
+            title: "title",
+            genre: "genre",
+            year: "year",
+            director: "director"
+        }
+    })
+    .then(res => this.setState({ movies: res.data }));
+}
+    
 
 function afterOpenModal() {
   // references are now sync'd and can be accessed.
@@ -29,23 +46,28 @@ return (
       onAfterOpen={afterOpenModal}
       onRequestClose={closeModal}
       style={customStyles}
-      contentLabel="Example Modal"
+      contentLabel="Movie Modal"
     >
 
       <h2 ref={_subtitle => (subtitle = _subtitle)}></h2>
       <button onClick={closeModal}>X</button>
 
-      <form class="modalForm" action="" method="post" enctype="multipart/form-data">
+      <form class="modalForm" action={postMovie} enctype="multipart/form-data">
     <h1 class="title">Add Movie</h1>
     
       <label for="name">Title:</label>
-      <input type="text" id="name" name="name" class="formInput"/>
+      <input type="text" id="title" name="title" class="formInput"/>
+
+      <label for="genre">Genre:</label>
+      <input type="text" id="genre" name="genre" class="formInput"/>
       
       <label for="year">Year:</label>
       <input type="text" id="year" name="year" class="formInput"/>
       
-      <label for="genre">Genre:</label>
-      <input type="text" id="genre" name="genre" class="formInput"/>
+      <label for="year">Director:</label>
+      <input type="text" id="director" name="director" class="formInput"/>
+      
+
 
     <button class="addBtn" type="submit">Add</button>
   </form>
